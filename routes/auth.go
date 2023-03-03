49aeed210d2f9a9aae6e75a48d9909925931adde
ichthoth <shalomtata334@gmail.com>
2023-03-03 19:51:36 +0100

    defined authentication routes

--- routes/auth.go
+++ routes/auth.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ichthoth/jwt-auth/controllers"
+)
+
+//Authentication routes
+func AuthRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("users/signup", controllers.SignUp())
+	incomingRoutes.POST("users/login", controllers.Login())
+
+}
