82df2910c2a4eb42c0db0efcf20cff0b19b23a44
ichthoth <shalomtata334@gmail.com>
2023-03-03 19:53:57 +0100

    user routes

--- routes/user.go
+++ routes/user.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ichthoth/jwt-auth/controllers"
+	"github.com/ichthoth/jwt-auth/middleware"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controllers.GetUser())
+	incomingRoutes.GET("/users/:user_id", controllers.GetUserID())
+}
