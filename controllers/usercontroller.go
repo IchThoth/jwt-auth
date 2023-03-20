0914e58002f46342ad2ccade5da7280f28e77b0a
ichthoth <shalomtata334@gmail.com>
2023-03-20 21:04:05 +0100

    getuser function

---
+++ controllers/usercontroller.go
@@ -0,0 +1,57 @@
+package controllers
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/ichthoth/jwt-auth/database"
+	"github.com/ichthoth/jwt-auth/helpers"
+	"github.com/ichthoth/jwt-auth/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var validate *validator.Validate = validator.New()
+
+func PasswordHash() {
+
+}
+
+func VerifyPassword() {
+
+}
+
+func SignUp() {
+
+}
+
+func Login() {
+
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
+		if err := helpers.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		var users models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&users)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, users)
+	}
+}
+
+func GetUsers() {
+
+}
